62ec3bc627a7ac0d12025d35880373d53d4fa62c
aero-the-synaptic-electrician <51681361+aero-the-synaptic-electrician@users.noreply.github.com>
2024-08-16 13:39:32 +0100

    Delete release directory

--- release/8a9474904a5b7dd09050.js
+++
@@ -1,1 +0,0 @@
-onmessage=e=>{e=e.data;let a={url:e};fetch(e,{mode:"cors"}).then(e=>e.blob()).then(e=>createImageBitmap(e)).then(e=>{a.image=e}).catch(()=>{a.errored=!0}).finally(()=>{self.postMessage(a)})};
